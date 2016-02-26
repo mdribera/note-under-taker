05445c2a02a805e52a7a010a4feebc34c9e4029b
Mark Ribera <markdribera@gmail.com>
2016-02-26 15:35:40 -0800

    active highlighting in menu

--- notes/templatetags/label_list_nav.py
+++ notes/templatetags/label_list_nav.py
@@ -5,5 +5,9 @@ from notes.models import Label
 register = template.Library()
 
 @register.inclusion_tag('notes/label_list_nav.html')
-def label_list_nav():
-	return {'labels': Label.objects.all()}
+def label_list_nav(request):
+	curr_label = request.GET.get('label')
+	return {
+		'labels': Label.objects.all(),
+		'curr_label': curr_label
+	}
--- notes/templatetags/label_list_note.py
+++ notes/templatetags/label_list_note.py
@@ -1,7 +1,10 @@
 from django import template
 
+from notes.models import Label
+
 register = template.Library()
 
 @register.inclusion_tag('notes/label_list_note.html')
 def label_list_note(labels):
+	labels = labels or Label.objects.all()
 	return {'labels': labels}
